3a576658aa28e62506b6e2bf0974bfb82e2c2c1c
Palak Singh <palaksingh9503@gmail.com>
2026-07-09 08:23:23 -0700

    Added LeetCode 83 - Remove Duplicates from Sorted List

---
+++ Easy/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,66 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {}
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class RemoveDuplicatesFromSortedList {
+
+    public ListNode deleteDuplicates(ListNode head) {
+
+        ListNode current = head;
+
+        while (current != null && current.next != null) {
+
+            if (current.val == current.next.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+
+        return head;
+    }
+
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val);
+
+            if (head.next != null) {
+                System.out.print(" -> ");
+            }
+
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        RemoveDuplicatesFromSortedList obj = new RemoveDuplicatesFromSortedList();
+
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(1);
+        head.next.next = new ListNode(2);
+        head.next.next.next = new ListNode(3);
+        head.next.next.next.next = new ListNode(3);
+
+        System.out.println("Original List:");
+        printList(head);
+
+        head = obj.deleteDuplicates(head);
+
+        System.out.println("After Removing Duplicates:");
+        printList(head);
+    }
+}
