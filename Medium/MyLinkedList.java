f1145424f589a6b2208b41855e3ffac5146c2493
Palak Singh <palaksingh9503@gmail.com>
2026-07-13 09:35:23 -0700

    Added LeetCode 707 - Design Linked List

---
+++ Medium/MyLinkedList.java
@@ -0,0 +1,121 @@
+
+public class MyLinkedList {
+
+    class Node {
+        int val;
+        Node next;
+
+        Node(int val) {
+            this.val = val;
+        }
+    }
+
+    private Node head;
+    private int size;
+
+    public MyLinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    public int get(int index) {
+
+        if (index < 0 || index >= size)
+            return -1;
+
+        Node curr = head;
+
+        for (int i = 0; i < index; i++)
+            curr = curr.next;
+
+        return curr.val;
+    }
+
+    public void addAtHead(int val) {
+
+        Node node = new Node(val);
+
+        node.next = head;
+        head = node;
+
+        size++;
+    }
+
+    public void addAtTail(int val) {
+
+        Node node = new Node(val);
+
+        if (head == null) {
+            head = node;
+        } else {
+
+            Node curr = head;
+
+            while (curr.next != null)
+                curr = curr.next;
+
+            curr.next = node;
+        }
+
+        size++;
+    }
+
+    public void addAtIndex(int index, int val) {
+
+        if (index < 0 || index > size)
+            return;
+
+        if (index == 0) {
+            addAtHead(val);
+            return;
+        }
+
+        Node curr = head;
+
+        for (int i = 0; i < index - 1; i++)
+            curr = curr.next;
+
+        Node node = new Node(val);
+
+        node.next = curr.next;
+        curr.next = node;
+
+        size++;
+    }
+
+    public void deleteAtIndex(int index) {
+
+        if (index < 0 || index >= size)
+            return;
+
+        if (index == 0) {
+            head = head.next;
+            size--;
+            return;
+        }
+
+        Node curr = head;
+
+        for (int i = 0; i < index - 1; i++)
+            curr = curr.next;
+
+        curr.next = curr.next.next;
+
+        size--;
+    }
+
+    public static void main(String[] args) {
+
+        MyLinkedList list = new MyLinkedList();
+
+        list.addAtHead(1);
+        list.addAtTail(3);
+        list.addAtIndex(1, 2);
+
+        System.out.println(list.get(1));
+
+        list.deleteAtIndex(1);
+
+        System.out.println(list.get(1));
+    }
+}
